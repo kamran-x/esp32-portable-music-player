0c656014c054805984cbd9e1c793039d06cb24d6
Kamran <thekamran.dev@gmail.com>
2026-04-25 20:52:41 -0400

    smooth gui interface + removing EEPROM

--- versions/KamranGames/KamranGames.ino
+++ versions/KamranGames/KamranGames.ino
@@ -1,7 +1,7 @@
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <EEPROM.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -17,12 +17,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 enum AppState { MENU, TETRIS, SNAKE, BLASTER, PACMAN, FLAPPY, DINO };
 AppState appState = MENU;
 
-// EEPROM addresses for hi-scores
-#define EE_TETRIS_HI  0
-#define EE_SNAKE_HI   2
-#define EE_BLASTER_HI 4
-
-// Include games after display/pins are declared
+// Include games
 #include "tetris.h"
 #include "snake.h"
 #include "blaster.h"
@@ -31,17 +26,18 @@ AppState appState = MENU;
 #include "dino.h"
 
 // --- Menu ---
-const char* gameNames[] = { "TETRIS", "SNAKE", "BLASTER", "PACMAN", "FLAPPY", "DINO"  };
+const char* gameNames[] = { "TETRIS", "SNAKE", "BLASTER", "PACMAN", "FLAPPY", "DINO" };
 const int NUM_GAMES = 6;
+
 int menuSel = 0;
 uint32_t lastMenuInput = 0;
 
-// Back-button: hold ROT for 2 seconds in-game → return to menu
-uint32_t rotHeldSince = 0;
-bool rotWasHeld = false;
+// Smooth scrolling
+float smoothOffset = 0;
+int targetOffset = 0;
+const int VISIBLE_ITEMS = 3;
 
-
-
+// --- DRAW MENU ---
 void drawMenu() {
   display.clearDisplay();
   display.setTextColor(WHITE);
@@ -52,61 +48,86 @@ void drawMenu() {
   display.print(F("KAMRAN GAMES"));
   display.drawFastHLine(0, 11, 128, WHITE);
 
-  // Game list
+  // Smooth animation
+  smoothOffset += (targetOffset - smoothOffset) * 0.2;
+
+  // Draw items
   for (int i = 0; i < NUM_GAMES; i++) {
-    int y = 16 + i * 14;
+    float yFloat = 16 + (i - smoothOffset) * 14;
+    int y = (int)yFloat;
+
+    if (y < -12 || y > 64) continue;
+
     if (i == menuSel) {
       display.fillRect(0, y, 128, 12, WHITE);
       display.setTextColor(BLACK);
     } else {
       display.setTextColor(WHITE);
     }
-    display.setCursor(8, y+1);
-    display.print(gameNames[i]);
 
-    // Show hi-score next to name
-    display.setCursor(70, y);
-    uint16_t hi = 0;
-    if (i == 0) EEPROM.get(EE_TETRIS_HI,  hi);
-    if (i == 1) EEPROM.get(EE_SNAKE_HI,   hi);
-    if (i == 2) EEPROM.get(EE_BLASTER_HI, hi);
+    display.setCursor(8, y + 1);
+    display.print(gameNames[i]);
   }
+
   display.display();
 }
 
+// --- INPUT ---
 void menuInput() {
   if (millis() - lastMenuInput < 200) return;
-  if (digitalRead(BTN_R) == LOW) { menuSel = (menuSel + NUM_GAMES - 1) % NUM_GAMES; lastMenuInput = millis(); }
-  if (digitalRead(BTN_L) == LOW) { menuSel = (menuSel + 1) % NUM_GAMES;              lastMenuInput = millis(); }
+
+  if (digitalRead(BTN_R) == LOW) {
+    menuSel = (menuSel + NUM_GAMES - 1) % NUM_GAMES;
+    lastMenuInput = millis();
+  }
+
+  if (digitalRead(BTN_L) == LOW) {
+    menuSel = (menuSel + 1) % NUM_GAMES;
+    lastMenuInput = millis();
+  }
+
+  // Scroll follow
+  if (menuSel < targetOffset) {
+    targetOffset = menuSel;
+  }
+  if (menuSel >= targetOffset + VISIBLE_ITEMS) {
+    targetOffset = menuSel - VISIBLE_ITEMS + 1;
+  }
+
+  // Select
   if (digitalRead(BTN_ROT) == LOW) {
     lastMenuInput = millis();
-    if      (menuSel == 0) { tetrisStart();  appState = TETRIS;  }
-    else if (menuSel == 1) { snakeStart();   appState = SNAKE;   }
+    if      (menuSel == 0) { tetrisStart();  appState = TETRIS; }
+    else if (menuSel == 1) { snakeStart();   appState = SNAKE; }
     else if (menuSel == 2) { blasterStart(); appState = BLASTER; }
-    else if (menuSel == 3) { pacmanStart(); appState = PACMAN; }
-    else if (menuSel == 4) { flappyStart(); appState = FLAPPY; }
-    else if (menuSel == 5) { dinoStart(); appState = DINO; }
-
+    else if (menuSel == 3) { pacmanStart();  appState = PACMAN; }
+    else if (menuSel == 4) { flappyStart();  appState = FLAPPY; }
+    else if (menuSel == 5) { dinoStart();    appState = DINO; }
   }
 }
 
+// --- SETUP ---
 void setup() {
   Wire.begin(SDA_PIN, SCL_PIN);
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+
   pinMode(BTN_ROT, INPUT_PULLUP);
   pinMode(BTN_R,   INPUT_PULLUP);
   pinMode(BTN_L,   INPUT_PULLUP);
+
   randomSeed(analogRead(0));
 }
 
+// --- LOOP ---
 void loop() {
   switch (appState) {
     case MENU:    menuInput(); drawMenu(); break;
     case TETRIS:  tetrisLoop(); break;
     case SNAKE:   snakeLoop(); break;
     case BLASTER: blasterLoop(); break;
-    case PACMAN: pacmanLoop(); break;
-    case FLAPPY: flappyLoop(); break;
-    case DINO: dinoLoop(); break;
+    case PACMAN:  pacmanLoop(); break;
+    case FLAPPY:  flappyLoop(); break;
+    case DINO:    dinoLoop(); break;
   }
 }
+
